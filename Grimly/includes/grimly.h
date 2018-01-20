7f30e148add22e0cb2aab41b139b0de5f142c0cc
Ted <marixalijustice@gmail.com>
2018-01-20 13:16:36 -0800

    Fixed tabulations

--- Grimly/includes/grimly.h
+++ Grimly/includes/grimly.h
@@ -6,7 +6,7 @@
 /*   By: ttran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 14:56:09 by ttran             #+#    #+#             */
-/*   Updated: 2018/01/19 14:56:10 by ttran            ###   ########.fr       */
+/*   Updated: 2018/01/20 13:13:12 by ttran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,40 @@
 # define GRIMLY_H
 # include <libft.h>
 
-typedef struct s_key
+typedef struct		s_key
 {
-    char *key;
-    int row;
-    int column;
-    int **arr;
-    int startx;
-    int starty;
-}               t_key;
+	char			*key;
+	int				row;
+	int				column;
+	int				**arr;
+	int				startx;
+	int				starty;
+}					t_key;
 
-typedef struct s_rs
+typedef struct		s_rs
 {
-    int run;
-    int n;
-    int exits;
-    int entrance;
-}   t_rs;
+	int				run;
+	int				n;
+	int				exits;
+	int				entrance;
+}					t_rs;
 
-typedef struct s_bfs
+typedef struct		s_bfs
 {
-    int x;
-    int y;
-    struct s_bfs *parent;
-}               t_bfs;
+	int				x;
+	int				y;
+	struct s_bfs	*parent;
+}					t_bfs;
 
-typedef struct s_queue
+typedef struct		s_queue
 {
-    struct s_bfs *node;
-    struct s_queue *next;
-}               t_queue;
+	struct s_bfs	*node;
+	struct s_queue	*next;
+}					t_queue;
 
-int main(int argc, char **argv);
-int dup_check(char *str);
-int validmaze(int fd);
-int solvemaze(t_key *key);
+int					main(int argc, char **argv);
+int					dup_check(char *str);
+int					validmaze(int fd);
+int					solvemaze(t_key *key);
 
 #endif
--- Grimly/srcs/dup_check.c
+++ Grimly/srcs/dup_check.c
@@ -6,36 +6,36 @@
 /*   By: ttran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 18:37:03 by ttran             #+#    #+#             */
-/*   Updated: 2018/01/18 18:37:05 by ttran            ###   ########.fr       */
+/*   Updated: 2018/01/20 13:16:27 by ttran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "grimly.h"
 
-int dup_check(char *str)
+int	dup_check(char *str)
 {
-    char arr[128];
-    int i;
+	char	arr[128];
+	int		i;
 
-    i = 0;
-    while (i < 128)
-        arr[i++] = 0;
-    i = 0;
-    while (str[i])
-    {
-        if (!(arr[(int)str[i]]))
-            arr[(int)str[i]] = str[i];
-        i++;
-    }
-    while (*str)
-    {
-        if (arr[(int)*str])
-        {
-            arr[(int)*str] = 0;
-            str++;
-        } 
-        else
-            return (1);
-    }
-    return (0);
+	i = 0;
+	while (i < 128)
+		arr[i++] = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (!(arr[(int)str[i]]))
+			arr[(int)str[i]] = str[i];
+		i++;
+	}
+	while (*str)
+	{
+		if (arr[(int)*str])
+		{
+			arr[(int)*str] = 0;
+			str++;
+		}
+		else
+			return (1);
+	}
+	return (0);
 }
