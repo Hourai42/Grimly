61ce4fc6a627c3a254d2dccc2fb3ff112cac36c2
Ted <marixalijustice@gmail.com>
2018-01-21 15:40:46 -0800

    ok

--- grimly/srcs/pop_queue.c
+++ grimly/srcs/pop_queue.c
@@ -6,21 +6,21 @@
 /*   By: ttran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 16:21:58 by ttran             #+#    #+#             */
-/*   Updated: 2018/01/20 16:21:59 by ttran            ###   ########.fr       */
+/*   Updated: 2018/01/21 15:40:05 by ttran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "grimly.h"
 
-t_bfs *pop_queue(t_queue **queue)
+t_bfs		*pop_queue(t_queue **queue)
 {
-	t_queue *fuckhead;
+	t_queue	*fuckhead;
 
 	fuckhead = NULL;
 	if ((*queue)->head->prev == NULL)
 	{
 		fuckhead = *queue;
-		(*queue)->head = NULL; 
+		(*queue)->head = NULL;
 		*queue = NULL;
 	}
 	else
